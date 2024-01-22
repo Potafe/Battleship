320a44a723952cf074e067f759c70e48e31b0960
Potafe <bt21me011@nitmz.ac.in>
2024-01-22 15:03:27 +0530

    change: move common functions to functions module

--- src/DOMS/battle.js
+++ src/DOMS/battle.js
@@ -1,7 +1,6 @@
+import functions from "./functions"
+
 const battle = (() => {
-
-    const BOARD_SIZE = 10
-
 
     const loadBoardTitle = (container, title) => {
         const boardTitle = document.createElement('h2')
@@ -11,72 +10,7 @@ const battle = (() => {
         container.appendChild(boardTitle)
     }
 
-    const loadCoordinates = (container, orientation) => {
-
-      if (orientation === 'X') {
-        const numberContainerX = document.createElement('div')
-        numberContainerX.classList = 'numberx-container'
-
-        const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-        numbers.forEach((element) => {
-          const number = document.createElement('div')
-          number.classList = 'number'
-          number.textContent = element
-          numberContainerX.appendChild(number)
-        }) 
-        
-        container.appendChild(numberContainerX)
-      }
-      
-      else if (orientation === 'Y') {
-        const numberContainerY = document.createElement('div')
-        numberContainerY.classList = 'numbery-container'
-
-        const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-        numbers.forEach((element) => {
-          const number = document.createElement('div')
-          number.classList = 'number'
-          number.textContent = element
-          numberContainerY.appendChild(number)
-        }) 
-        
-        container.appendChild(numberContainerY)
-
-      }
-    }
-
-
-    const loadField = (container) => {
-      const fieldContainer = document.createElement('field-container')
-      fieldContainer.className = 'field-container'
-
-      for (let i = 0; i < BOARD_SIZE; i += 1) {
-        for (let j = 0; j < BOARD_SIZE; j += 1) {
-          const field = document.createElement('div')
-          field.className = 'field'
-  
-          fieldContainer.appendChild(field)
-        }
-      }
-
-      container.appendChild(fieldContainer)
-    }
-
-    const loadBoard = (container, friendlyOrEnemy) => {
-        const board = document.createElement('div')
-        
-        board.id = `board-${friendlyOrEnemy}`
-        board.classList.add('board', friendlyOrEnemy)
-        
-        loadCoordinates(board, 'X')
-        loadCoordinates(board, 'Y')
-        loadField(board)
-
-        container.appendChild(board)
-    }
-
+    
     const loadBoardsSection = () => {
         const app = document.getElementById('app')
     
@@ -89,8 +23,8 @@ const battle = (() => {
         loadBoardTitle(friendlyBoardContainer, 'FRIENDLY WATERS')
         loadBoardTitle(enemyBoardContainer, 'ENEMY WATERS')
     
-        loadBoard(friendlyBoardContainer, 'friendly')
-        loadBoard(enemyBoardContainer, 'enemy')
+        functions.loadBoard(friendlyBoardContainer, 'friendly')
+        functions.loadBoard(enemyBoardContainer, 'enemy')
     
         boardsSection.appendChild(friendlyBoardContainer)
         boardsSection.appendChild(enemyBoardContainer)
---
+++ src/DOMS/functions.js
@@ -0,0 +1,76 @@
+    
+const functions = (() => {
+
+   
+    const BOARD_SIZE = 10
+
+    const loadCoordinates = (container, orientation) => {
+
+        if (orientation === 'X') {
+        const numberContainerX = document.createElement('div')
+        numberContainerX.classList = 'numberx-container'
+
+        const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+        numbers.forEach((element) => {
+            const number = document.createElement('div')
+            number.classList = 'number'
+            number.textContent = element
+            numberContainerX.appendChild(number)
+        }) 
+        
+        container.appendChild(numberContainerX)
+        }
+        
+        else if (orientation === 'Y') {
+        const numberContainerY = document.createElement('div')
+        numberContainerY.classList = 'numbery-container'
+
+        const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+        numbers.forEach((element) => {
+            const number = document.createElement('div')
+            number.classList = 'number'
+            number.textContent = element
+            numberContainerY.appendChild(number)
+        }) 
+        
+        container.appendChild(numberContainerY)
+
+        }
+    }
+
+
+    const loadField = (container) => {
+        const fieldContainer = document.createElement('field-container')
+        fieldContainer.className = 'field-container'
+
+        for (let i = 0; i < BOARD_SIZE; i += 1) {
+        for (let j = 0; j < BOARD_SIZE; j += 1) {
+            const field = document.createElement('div')
+            field.className = 'field'
+
+            fieldContainer.appendChild(field)
+        }
+        }
+
+        container.appendChild(fieldContainer)
+    }
+
+    const loadBoard = (container, friendlyOrEnemy) => {
+        const board = document.createElement('div')
+        
+        board.id = `board-${friendlyOrEnemy}`
+        board.classList.add('board', friendlyOrEnemy)
+        
+        loadCoordinates(board, 'X')
+        loadCoordinates(board, 'Y')
+        loadField(board)
+
+        container.appendChild(board)
+    }
+
+    return  { loadBoard }
+}) 
+
+export default functions
