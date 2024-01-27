719eb4af44dec711f80ff9d0deafaaafb8b5c597
Potafe <bt21me011@nitmz.ac.in>
2024-01-27 15:37:01 +0530

    refactor: clean page module

--- src/DOMS/page.js
+++ src/DOMS/page.js
@@ -1,31 +1,14 @@
 import start from "./start";
-import battle from "./battle";
-import Gameloop from "../factories/gameloop";
 import functions from "./functions";
 import setup from "./setup";
-import fleet from "./fleet";
-import Ship from "../factories/ship";
 import drag from "./drag";
 
 const page = (() => {
-  const game = Gameloop.initializeGame();
-
-  const found = {
-    carrier: false,
-    battleship: false,
-  }
-
-  const shipOnDrag = {
-    name: '',
-    length: '',
-  }
-
-
+  
   const loadSetup = () => {
     functions.deleteContent()
     setup.loadSetupMaterial()
-    console.log(game.player.getMap())
-    drag.draggableFields(shipOnDrag, found, game.player.getMap())
+    drag.draggableFields()
   }
 
   const playButton = () => {
@@ -38,32 +21,6 @@ const page = (() => {
     playButton();
   };
 
-  const handleFieldClick = (event) => {
-    const { target } = event
-
-    const index = [...target.parentNode.children].indexOf(target)
-    const x = parseInt(index / 10, 10)
-    const y = index % 10
-
-    const friendlyMap = game.player.board.getBoard()
-    const enemyMap = game.cpu.board.getBoard()
-
-    if (friendlyMap[x][y] === 'hit' || friendlyMap[x][y] === 'missed') return
-
-    if (friendlyMap[x][y] === 'x') {
-      console.log('b')
-      target.style.backgroundColor = 'lightblue'
-    } else {
-      console.log('r')
-      target.classList.add('hit')
-    }
-  }
-
-  const initBoardFields = (boardNode) => {
-    boardNode.childNodes.forEach((field) => {
-      field.addEventListener('click', handleFieldClick)
-    })
-  }
 
   return { loadContent };
 })();
